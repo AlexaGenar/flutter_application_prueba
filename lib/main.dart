d6ad839d60b8c769a2eb873d1a923b73dca2ee8d
Alexa Genar <gomezgenesisalexa03@gmail.com>
2025-03-20 10:42:40 -0600

    Lab teminado

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  //_ barra baja clase privada
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
@@ -26,39 +27,136 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
-    // ↓ Add this.
+ 
   void getNext() {
     current = WordPair.random();
     notifyListeners();
   }
+
+  var favorites = <WordPair>[];// funcion boton me gusta
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
 }
 
-class MyHomePage extends StatelessWidget {
+// Agregamos dos pagina y riel de navegacion 
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+var selectedIndex = 0;
+
+@override
+Widget build(BuildContext context) {
+Widget page;  // asigna una pantalla a page
+switch (selectedIndex) {
+  case 0:
+    page = GeneratorPage();
+    break;
+  case 1:
+    page = FavoritesPage();   //cambiarlo por favorite
+    break;
+  default:
+    throw UnimplementedError('no widget for $selectedIndex');
+}
+
+    return LayoutBuilder(   //El usuario cambia el tamaño de la ventana de la app.
+      builder: (context, constraints) {
+        return Scaffold(
+          body: Row(
+            children: [
+              SafeArea(
+                child: NavigationRail(  //riel de navegacion
+                  extended: constraints.maxWidth >= 600,     //extended: constraints.maxWidth >= 600 hace que sea responsive
+                    //quitar o poner etiquetas de los extends= true pone etiqueta, label: Text('Home'),
+                  destinations: [
+                    NavigationRailDestination(
+                      icon: Icon(Icons.home),
+                      label: Text('Home'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.favorite),
+                      label: Text('Favorites'),
+                    ),
+                  ],
+                  selectedIndex: selectedIndex,
+                  onDestinationSelected: (value) {
+                    setState(() {
+                      selectedIndex = value;
+                    });
+                    
+                  },
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                  child: page,
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+    );
+}
+}
+
+
+class GeneratorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
-    var pair = appState.current;  
+    var pair = appState.current;
 
-    return Scaffold(
-      body: Center(    //centrar cosas 
-        child: Column(
-           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-           // Text('A random AWESOME idea:'),// texto x
-             BigCard(pair: pair),  //card
-              SizedBox(height: 10),
-           ElevatedButton(
-        onPressed: () {
-          appState.getNext();  // ← This instead of print().
-        },
-        child: Text('Next'),
-            ),
-          ],
-        ),
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center, //Espacio entre los objetos 
+        children: [
+          BigCard(pair: pair),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite();
+                },
+                icon: Icon(icon),
+                label: Text('Like'),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text('Next'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
 }
+
+// ...
 
 class BigCard extends StatelessWidget {
   const BigCard({
@@ -90,3 +188,35 @@ class BigCard extends StatelessWidget {
     );
   }
 }
+// ...
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+              '${appState.favorites.length} favorites:'),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          ),
+      ],
+    );
+  }
+}
+
+
+
